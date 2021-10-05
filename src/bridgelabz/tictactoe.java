12394ce6377465f2bde42fe7be63909f0184ac20
atif00 <mohammedatif90@gmail.com>
2021-10-05 21:24:02 +0530

    ADD display the current board

--- src/bridgelabz/tictactoe.java
+++ src/bridgelabz/tictactoe.java
@@ -18,11 +18,18 @@ public class tictactoe {
         System.out.println("Enter your choice  X or O only");
         return scanner.next().toUpperCase().charAt(0);
     }
+    public static void showBoard() {
+
+        System.out.println(gameArray[0] + " | " + gameArray[1] + " | " + gameArray[2]);
+        System.out.println(gameArray[3] + " | " + gameArray[4] + " | " + gameArray[5]);
+        System.out.println(gameArray[6] + " | " + gameArray[7] + " | " + gameArray[8]);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         ticTacToe();
         userTurn = user(scanner);
         computerTurn = (userTurn == 'X') ? 'O' : 'X';
+        showBoard();
     }
 }
