a82f7613fd01a5023f99f4bc4dd41be6c6e39572
atif00 <mohammedatif90@gmail.com>
2021-10-05 21:15:26 +0530

    ADD allow player to choose letter X or O

--- src/bridgelabz/tictactoe.java
+++ src/bridgelabz/tictactoe.java
@@ -1,17 +1,28 @@
 package bridgelabz;
 
+import java.util.Scanner;
+
 public class tictactoe {
+    private static char userTurn;
+    private static char computerTurn;
+    public static char[] gameArray = new char[10];
 
     public static void ticTacToe() {
-
-        char gameArray[] = new char[10];
         for (int i = 0; i < gameArray.length; i++) {
 
             gameArray[i] = ' ';
         }
     }
 
+    public static char user(Scanner scanner) {
+        System.out.println("Enter your choice  X or O only");
+        return scanner.next().toUpperCase().charAt(0);
+    }
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ticTacToe();
+        userTurn = user(scanner);
+        computerTurn = (userTurn == 'X') ? 'O' : 'X';
     }
 }
