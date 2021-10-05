4be980933dc6208e00df50e6745f14c2b8c3d0b9
atif00 <mohammedatif90@gmail.com>
2021-10-05 14:59:54 +0530

    ADD Initialize Board

---
+++ src/bridgelabz/tictactoe.java
@@ -0,0 +1,17 @@
+package bridgelabz;
+
+public class tictactoe {
+
+    public static void ticTacToe() {
+
+        char gameArray[] = new char[10];
+        for (int i = 0; i < gameArray.length; i++) {
+
+            gameArray[i] = ' ';
+        }
+    }
+
+    public static void main(String[] args) {
+        ticTacToe();
+    }
+}
